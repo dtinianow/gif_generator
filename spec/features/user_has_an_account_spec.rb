e3a8ac7f3cbbddf157337b57e429dc42f9204672
David Tinianow <david.tinianow@gmail.com>
2016-07-23 11:51:11 -0600

    add home page and buttons linking to it

--- spec/features/user_has_an_account_spec.rb
+++ spec/features/user_has_an_account_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "User has an account" do
-  scenario "they can create a new account" do
-    visit new_user_path
+  scenario "they can visit the homepage and create a new account" do
+    visit root_path
+
+    click_on "Create New Account"
+    expect(current_path).to eq (new_user_path)
 
     fill_in "Username", with: "david"
     fill_in "Password", with: "password"
@@ -18,7 +21,10 @@ RSpec.feature "User has an account" do
   scenario "they can login successfully" do
     user = User.create(username: "david", password: "password")
 
-    visit login_path
+    visit root_path
+
+    click_on "Login"
+    expect(current_path).to eq(login_path)
 
     fill_in "Username", with: "david"
     fill_in "Password", with: "password"
@@ -27,5 +33,23 @@ RSpec.feature "User has an account" do
 
     expect(current_path).to eq(user_path(user))
     expect(page).to have_content("Welcome, david")
+  end
+
+  scenario "they can logout successfully" do
+    user = User.create(username: "david", password: "password")
+
+    visit login_path
+
+    fill_in "Username", with: "david"
+    fill_in "Password", with: "password"
+
+    click_on "Login"
+
+    expect(page).to have_content("Welcome, david")
+
+    click_on "Logout"
+
+    expect(current_path).to eq(login_path)
+    expect(page).to_not have_content("david")
   end
 end
